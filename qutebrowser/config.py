a412cbeb64f3e14e4bb05cc7bf292188a44faaa2
Johannes Tynes <tynesjo@gmail.com>
2019-12-23 19:33:07 +0000

    Setting scrollbar off

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -11,6 +11,26 @@ def imap(key, command):
     """Bind key to command in insert mode."""
     bind(key, command, 'insert')
 
+def cmap(key, command):
+    """Bind key to command in command mode."""
+    bind(key, command, 'command')
+
+def tmap(key, command):
+    """Bind key to command in caret mode."""
+    bind(key, command, 'caret')
+
+def pmap(key, command):
+    """Bind key to command in passthrough mode."""
+    bind(key, command, 'passthrough')
+
+def unmap(key, mode):
+    """Unbind key in mode."""
+    config.unbind(key, mode=mode)
+
+def nunmap(key):
+    """Unbind key in normal mode."""
+    unmap(key, mode='normal')
+
 
 # Zooming
 nmap('<Ctrl-e>', 'zoom-in')
@@ -40,3 +60,9 @@ c.hints.auto_follow = 'always'
 c.editor.command = ['urxvt', '-e', 'env', 'TERM=xterm-256color', 'emacsclient', '-nw', '-s', 'instance1', '{}']
 nmap('gF', 'view-source --edit')
 imap('<Ctrl-i>', 'open-editor')
+
+# Colemak Customizations
+nmap('l', 'search-next')
+nmap('L', 'search-prev')
+
+c.scrolling.bar = 'never'
